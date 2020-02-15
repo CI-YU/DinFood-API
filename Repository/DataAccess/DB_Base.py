69958846425db567234c279196fbf9867a96fba4
JackChenAB <abc81825@twbbe.com>
2020-02-15 17:04:37 +0800

    更新DB_Base

--- Repository/DataAccess/DB_Base.py
+++ Repository/DataAccess/DB_Base.py
@@ -12,44 +12,44 @@ class DB ():
         self.myclient = MongoClient(
             host=pHost, port=pPort, username=pUsername, password=pPassword)
 
-    def Insert(self, data, pDBName='test', pCollectionName='students'):
-        db = self.myclient[pDBName]
+    def Insert(self, data, pTableName='test', pCollectionName='students'):
+        db = self.myclient[pTableName]
         collection = db[pCollectionName]
         result = collection.insert(data)
         return result
 
-    def SearchAll(self, pDBName='test', pCollectionName='students'):
-        db = self.myclient[pDBName]
+    def SearchAll(self, pTableName='test', pCollectionName='students'):
+        db = self.myclient[pTableName]
         collection = db[pCollectionName]
         d = collection.find()
         return d
 
-    def Search(self, pQuery, pDBName='test', pCollectionName='students'):
-        db = self.myclient[pDBName]
+    def Search(self, pQuery, pTableName='test', pCollectionName='students'):
+        db = self.myclient[pTableName]
         collection = db[pCollectionName]
         d = collection.find(pQuery)
         return d
 
-    def UpdateOne(self, pQuery, pNewVal, pDBName='test', pCollectionName='students'):
-        db = self.myclient[pDBName]
+    def UpdateOne(self, pQuery, pNewVal, pTableName='test', pCollectionName='students'):
+        db = self.myclient[pTableName]
         collection = db[pCollectionName]
         x = collection.update_one(pQuery, pNewVal)
         return x
 
-    def UpdateMany(self, pQuery, pNewVal, pDBName='test', pCollectionName='students'):
-        db = self.myclient[pDBName]
+    def UpdateMany(self, pQuery, pNewVal, pTableName='test', pCollectionName='students'):
+        db = self.myclient[pTableName]
         collection = db[pCollectionName]
         x = collection.update_many(pQuery, pNewVal)
         return x
 
-    def DeleteOne(self, pQuery, pNewVal, pDBName='test', pCollectionName='students'):
-        db = self.myclient[pDBName]
+    def DeleteOne(self, pQuery, pNewVal, pTableName='test', pCollectionName='students'):
+        db = self.myclient[pTableName]
         collection = db[pCollectionName]
         x = collection.delete_one(pQuery)
         return x
 
-    def DeleteMany(self, pQuery, pNewVal, pDBName='test', pCollectionName='students'):
-        db = self.myclient[pDBName]
+    def DeleteMany(self, pQuery, pNewVal, pTableName='test', pCollectionName='students'):
+        db = self.myclient[pTableName]
         collection = db[pCollectionName]
         x = collection.delete_many(pQuery)
         return x
