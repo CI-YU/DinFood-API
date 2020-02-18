d91a8e6f308b5304754f7dafa5406f1eee57c859
A00879 <yuan0304@gmail.com>
2020-02-18 09:23:34 +0800

    ff

--- Repository/DataAccess/DB_Base.py
+++ Repository/DataAccess/DB_Base.py
@@ -4,6 +4,8 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 #region 參數55s
 host = config['DATABASE']['HOST']
+host3 = config['DATABASE']['HOST']
+pp = host3
 port = int(config['DATABASE']['PORT'])
 username = config['DATABASE']['USERNAME']
 password = config['DATABASE']['PASSWORD']
