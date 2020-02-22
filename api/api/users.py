df559b51a9047121d414e77dd6e0dc53f8217912
JackChenAB <abc81825@gmail.com>
2020-02-22 16:00:32 +0800

    save

--- api/api/users.py
+++ api/api/users.py
@@ -6,9 +6,8 @@ from flask import request, g
 from . import Resource
 from .. import schemas
 
-
 class Users(Resource):
 
     def get(self):
 
-        return None, 200, None
+        return "123", 200, None
